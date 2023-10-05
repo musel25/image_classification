d9fbaccfc8dfb98f5b24938fb69416dce0c1e12f
musel25 <muselemmanuel@hotmail.com>
2023-10-04 20:03:09 -0600

    notebooks

---
+++ utils.py
@@ -0,0 +1,234 @@
+#para utilizar tensores etc
+import torch
+#para el modelo
+from torch import nn
+#para importar datasets
+import torchvision
+#para transformar imagenes
+import torchvision.transforms as transforms
+#para visualizaciones
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+
+def plot_sample_images(trainset, classes, rows, cols):
+    fig = plt.figure(figsize=(9, 9))
+    for i in range(1, rows * cols + 1):
+        random_idx = torch.randint(0, len(trainset), size=[1]).item()
+        img, label = trainset[random_idx]
+        fig.add_subplot(rows, cols, i)
+        plt.imshow(img.permute(1,2,0), cmap="gray")
+        plt.title(classes[label])
+        plt.axis(False)
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+def train_step(model: torch.nn.Module, 
+               dataloader: torch.utils.data.DataLoader, 
+               loss_fn: torch.nn.Module, 
+               optimizer: torch.optim.Optimizer,
+               device):
+    # Put model in train mode
+    model.train()
+    
+    # Setup train loss and train accuracy values
+    train_loss, train_acc = 0, 0
+    
+    # Loop through data loader data batches
+    for batch, (X, y) in enumerate(dataloader):
+        # Send data to target device
+        X, y = X.to(device), y.to(device)
+
+        # 1. Forward pass
+        y_pred = model(X)
+
+        # 2. Calculate  and accumulate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item() 
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+        # Calculate and accumulate accuracy metric across all batches
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+
+    # Adjust metrics to get average loss and accuracy per batch 
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+def test_step(model: torch.nn.Module, 
+              dataloader: torch.utils.data.DataLoader, 
+              loss_fn: torch.nn.Module,
+              device):
+    # Put model in eval mode
+    model.eval() 
+    
+    # Setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
+    
+    # Turn on inference context manager
+    with torch.inference_mode():
+        # Loop through DataLoader batches
+        for batch, (X, y) in enumerate(dataloader):
+            # Send data to target device
+            X, y = X.to(device), y.to(device)
+    
+            # 1. Forward pass
+            test_pred_logits = model(X)
+
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+            
+            # Calculate and accumulate accuracy
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
+            
+    # Adjust metrics to get average loss and accuracy per batch 
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+
+# 1. Take in various parameters required for training and test steps
+def train(device,
+          model: torch.nn.Module, 
+          train_dataloader: torch.utils.data.DataLoader, 
+          test_dataloader: torch.utils.data.DataLoader, 
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          epochs: int = 5, 
+          ):
+    
+    # 2. Create empty results dictionary
+    results = {"train_loss": [],
+        "train_acc": [],
+        "test_loss": [],
+        "test_acc": []
+    }
+    
+    # 3. Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer, device=device)
+        test_loss, test_acc = test_step(model=model,
+            dataloader=test_dataloader,
+            loss_fn=loss_fn, device=device)
+        
+        # 4. Print out what's happening
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        # 5. Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    # 6. Return the filled results at the end of the epochs
+    return results
+
+def make_predictions(model: torch.nn.Module, data: list, device: torch.device):
+    pred_probs = []
+    model.eval()
+    with torch.inference_mode():
+        for sample in data:
+            # Prepare sample
+            sample = torch.unsqueeze(sample, dim=0).to(device) # Add an extra dimension and send sample to device
+
+            # Forward pass (model outputs raw logit)
+            pred_logit = model(sample)
+
+            # Get prediction probability (logit -> prediction probability)
+            pred_prob = torch.softmax(pred_logit.squeeze(), dim=0) # note: perform softmax on the "logits" dimension, not "batch" dimension (in this case we have a batch size of 1, so can perform on dim=0)
+
+            # Get pred_prob off GPU for further calculations
+            pred_probs.append(pred_prob.cpu())
+
+    # Stack the pred_probs to turn list into a tensor
+    return torch.stack(pred_probs).argmax(dim=1)     # Turn the prediction probabilities into prediction labels by taking the argmax()
+
+def plot_loss_curves(results):
+    # Get the loss values of the results dictionary (training and test)
+    loss = results['train_loss']
+    test_loss = results['test_loss']
+
+    # Get the accuracy values of the results dictionary (training and test)
+    accuracy = results['train_acc']
+    test_accuracy = results['test_acc']
+
+    # Figure out how many epochs there were
+    epochs = range(len(results['train_loss']))
+
+    # Setup a plot 
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label='train_loss')
+    plt.plot(epochs, test_loss, label='test_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label='train_accuracy')
+    plt.plot(epochs, test_accuracy, label='test_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+def plot_predictions(test_samples,test_labels, classes, pred_classes):
+    plt.figure(figsize=(9, 9))
+    nrows = 3
+    ncols = 3
+    for i, sample in enumerate(test_samples):
+        # Create a subplot
+        plt.subplot(nrows, ncols, i+1)
+
+        # Plot the target image
+        plt.imshow(sample.permute(1,2,0), cmap="gray")
+
+        # Find the prediction label (in text form, e.g. "Sandal")
+        pred_label = classes[pred_classes[i]]
+
+        # Get the truth label (in text form, e.g. "T-shirt")
+        truth_label = classes[test_labels[i]] 
+
+        # Create the title text of the plot
+        title_text = f"Pred: {pred_label} | Truth: {truth_label}"
+        
+        # Check for equality and change title colour accordingly
+        if pred_label == truth_label:
+            plt.title(title_text, fontsize=10, c="g") # green text if correct
+        else:
+            plt.title(title_text, fontsize=10, c="r") # red text if wrong
+        plt.axis(False)
+        
+def mean_std(loader):
+  sum, squared_sum, num_batches = 0,0,0
+  for data,_ in loader:
+    sum += torch.mean(data,dim=[0,2,3])
+    squared_sum += torch.mean(data**2,dim=[0,2,3])
+    num_batches += 1
+  mean = sum/num_batches
+  std = (squared_sum/num_batches - mean**2)**0.5
+  return mean, std
